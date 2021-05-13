3aafed94e225446fe01ffc90d0c3749ac167394f
areph <40balmung@gmail.com>
2021-05-13 09:43:25 +0900

    success problem2 when POST method

--- server.js
+++ server.js
@@ -2,13 +2,20 @@
 
 const express = require('express');
 
-const PORT = 8080;
 const HOST = '0.0.0.0';
+const PORT = 8080;
 
 // App
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('{text: hello world}');
+});
+
+app.post('/', (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(PORT, HOST);
