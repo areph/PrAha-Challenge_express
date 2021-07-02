40662ad05214cb8f03b72c25d5cd100e45c6af08
areph <40balmung@gmail.com>
2021-07-02 18:12:24 +0900

    add set cookie test

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 // use static files
 app.use(express.static("public"));
 
+// set a cookie
+app.use((req, res, next) => {
+  const cookie_sigunature = req.cookies.signature;
+  if (cookie_sigunature === undefined) {
+    res.cookie('signature', 'meeee!', {maxAge: 1000, httpOnly: true})
+  }
+  console.log('cookie created successfully')
+})
+
 app.listen(PORT, HOST);
 console.log(
   `Running on http://${HOST === "0.0.0.0" ? "localhost" : HOST}:${PORT} ...`
