2dd28b1698fb03c8e7c26857203ce75882da85a4
areph <40balmung@gmail.com>
2021-07-07 18:15:31 +0900

    add api for GET posts

--- server.js
+++ server.js
@@ -6,23 +6,22 @@ const helmet = require('helmet');
 const HOST = '0.0.0.0';
 const PORT = 8080;
 
+// Blog posts
+const POSTS = {
+  1: { post: 'This is the first blog post.' },
+  2: { post: 'This is the second blog post.' },
+  3: { post: 'This is the third blog post.' },
+};
+
 // App
 const app = express();
 
 // use module
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('{text: hello world}');
-});
-
-app.post('/', (req, res) => {
-  if (req.get('Content-Type') === 'application/json') {
-    return res.status(201).send(req.body);
-  }
-  return res.sendStatus(400);
+app.get('/api/posts', (req, res) => {
+  res.json(POSTS);
 });
 
 app.listen(PORT, HOST);
