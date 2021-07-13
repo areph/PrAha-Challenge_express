0263f95054b7a64f1dc8ba112dfaed00f232e153
areph <40balmung@gmail.com>
2021-07-13 15:19:15 +0900

    add other express server

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
 'use strict';
 
 const express = require('express');
-const helmet = require('helmet');
 
 const HOST = '0.0.0.0';
 const PORT = 8080;
 
 // App
 const app = express();
+const app_nocache = express();
 
-// use module
-app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// use static files
+app.use(express.static('public'));
+app_nocache.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('{text: hello world}');
-});
-
-app.post('/', (req, res) => {
-  if (req.get('Content-Type') === 'application/json') {
-    return res.status(201).send(req.body);
-  }
-  return res.sendStatus(400);
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT} ...`);
+app.listen(PORT, () => console.log(`Running on http://${HOST}:${PORT} ...`));
+app_nocache.listen(PORT + 1, () =>
+  console.log(`Running on http://${HOST}:${PORT + 1} ...`)
+);
