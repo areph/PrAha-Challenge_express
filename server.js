11c99df0a3de0b00adb4aad68007d1bc47995123
areph <40balmung@gmail.com>
2021-07-13 15:32:26 +0900

    add nocahe image

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const PORT = 8080;
 const app = express();
 const app_nocache = express();
 
+const cacheHandler = (req, res, next) => {
+  res.set('Cache-Control', 'no-store');
+  res.set('Expires', '-1');
+  next();
+};
+app_nocache.use(cacheHandler);
+
 // use static files
 app.use(express.static('public'));
 app_nocache.use(express.static('public'));
